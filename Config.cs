d720e088d10f42fe3a4570e9a8ac907c33d2322c
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2026-09-16 20:47:33 +0200

    Changed BaseApiUrl to represent the reality

--- Config.cs
+++ Config.cs
@@ -11,7 +11,7 @@ public class Config
     public string Language { get; set; } = "english";
 
     [Description("Base API url for OverseerAC. *Do not edit unless you know what you're doing!*")]
-    public string BaseApiUrl { get; set; } = "https://overseerac.eu/api/";
+    public string BaseApiUrl { get; set; } = "https://api.overseerac.eu/v1/";
 
     [Description("Your OverseerAC API key, *DO NOT SHARE IT!*. If you don't have one, you can request it at our Discord, which can be found here: https://github.com/GraczBezNicku/OverseerAC.Plugin.TrustFactor")]
     public string ApiKey { get; set; } = "";
